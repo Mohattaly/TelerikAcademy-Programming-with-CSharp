2bd9afbf5d8210673e787f81916f58e5eaf72850
Mohattaly <mohattaly@gmail.com>
2016-06-03 17:02:58 +0200

    ex 2

--- Ch02-Primitive Types and Variables/02.AssignValuesToVariables/Program.cs
+++ Ch02-Primitive Types and Variables/02.AssignValuesToVariables/Program.cs
@@ -10,6 +10,16 @@ namespace _02.AssignValuesToVariables
     {
         static void Main(string[] args)
         {
+            // 2. Which of the following values can be assigned to variables of type float, double and decimal: 5, -5.01, 34.567839023; 12.345; 8923.1234857; 3456.091124875956542151256683467?
+
+
+            5
+            -5.01
+            34.567839023;
+            12.345;
+            8923.1234857;
+            3456.091124875956542151256683467;
+
         }
     }
 }
